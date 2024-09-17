36b53827f6f92da93f624e957599539252f8efbf
eliott  peron <eliott.peron@epfedu.fr>
2024-09-17 16:51:27 +0200

    dans ce project j'ai suivie les conigne qui m'on permit d'apprendre les base de java

---
+++ ex01/src/ex01/Ex01.java
@@ -0,0 +1,33 @@
+/* // peron eliott TDC 17/09
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ex01;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author eliot
+ */
+public class Ex01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // peron eliott TDC 17/09
+        System.out.println("Bonjour");
+        System.out.println("Au'revoir");
+        
+        
+        String prenom;
+        Scanner sc;
+        sc = new Scanner(System.in);
+        System.out.println("Bonjour, quel est votre prenom ?");
+        prenom = sc.nextLine();
+        
+        
+    }
+    
+}
