d465e540241a1dfcfeb8a7791e34c8cec0278b3d
eliott  peron <eliott.peron@epfedu.fr>
2024-10-16 12:58:02 +0200

    exo 2 pas fini

---
+++ TP2_convertisseurObjet_PERON/src/tp2_convertisseurobjet_peron/TP2_convertisseurObjet_PERON.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_convertisseurobjet_peron;
+
+/**
+ *
+ * @author eliot
+ */
+public class TP2_convertisseurObjet_PERON {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+       convertiseur convertiseur1 = new convertiseur();
+    }
+    
+}
