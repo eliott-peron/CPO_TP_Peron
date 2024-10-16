83804be1673e1d15461200a3637577328ee56548
eliott  peron <eliott.peron@epfedu.fr>
2024-10-16 12:24:11 +0200

    exercice fenetre1 fini

---
+++ TP2_exo0_PERON/src/tp2_exo0_peron/TP2_exo0_PERON.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_exo0_peron;
+
+/**
+ *
+ * @author eliot
+ */
+public class TP2_exo0_PERON {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
