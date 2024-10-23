3be8e4483f660fac4b3ac05b1e4a3b81d133b8ea
eliott  peron <eliott.peron@epfedu.fr>
2024-10-23 12:20:03 +0200

    exo 2 pas pas fini

---
+++ TP2_convertisseurObjet_PERON/src/tp2_convertisseurobjet_peron/convertisseur.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_convertisseurobjet_peron;
+
+/**
+ *
+ * @author eliot
+ */
+public class convertisseur {
+
+    int nbConversions;
+
+    public convertisseur() {
+        nbConversions = 0;
+    }
+
+    public double CelciusVersKelvin(double tCelcius) {
+        tCelcius = tCelcius + 273;
+        return tCelcius;
+         
+    }
+
+    public double KelvinVersCelcius(double tKelvin) {
+        tKelvin = tKelvin - 273;
+        nbConversions = nbConversions +1;
+        return tKelvin;
+         
+    }
+
+    public double FarenheitVersCelcius(double Farenhei) {
+        Farenhei = (Farenhei - 32) * 1.8;
+        nbConversions = nbConversions +1;
+        return Farenhei;
+    }
+
+    public double CelciusVersFarenheit(double celciuse) {
+        celciuse = (celciuse * 1.8) + 32;
+        nbConversions = nbConversions +1;
+        return celciuse;
+    }
+
+    public double KelvinVersFarenheit(double kelvin) {
+        double celsius = KelvinVersCelcius(kelvin);
+        double fahrenheit = CelciusVersFarenheit(celsius);
+        nbConversions = nbConversions +1;
+        return fahrenheit;
+    }
+
+    public double FarenheitVersKelvin(double Farenheit) {
+        double celsius = FarenheitVersCelcius(Farenheit);
+        double Kelvin = CelciusVersKelvin(celsius);
+        nbConversions = nbConversions +1;
+        return Kelvin;
+    }
+
+    @Override
+    public String toString() {
+        return "nb de conversions " + nbConversions;
+    }
+
+}
