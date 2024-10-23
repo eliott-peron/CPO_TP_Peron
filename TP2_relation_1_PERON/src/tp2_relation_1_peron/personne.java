382ead6bbebc92fb51b288c30fa801cf786b09eb
eliott  peron <eliott.peron@epfedu.fr>
2024-10-23 16:41:05 +0200

    TP2 exo 4 pas encore fini

---
+++ TP2_relation_1_PERON/src/tp2_relation_1_peron/personne.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_relation_1_peron;
+
+/**
+ *
+ * @author eliot
+ */
+public class personne {
+    
+int nbpersonne;
+
+    public personne(int nbpersonne) {
+        this.nbpersonne = nbpersonne;
+    }
+    
+    
+
+
+
+}
---
+++ TP2_relation_1_PERON/src/tp2_relation_1_peron/voiture.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_relation_1_peron;
+
+/**
+ *
+ * @author eliot
+ */
+public class voiture {
+    
+    int nbvoitur;
+
+    public voiture(int nbvoitur) {
+        this.nbvoitur = nbvoitur;
+    }
+    
+    
+    
+    
+    
+}
